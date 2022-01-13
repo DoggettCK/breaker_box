473ead587088928d56f8bf34c060774e86b5d593
Gary Miller <gmiller@covermymeds.com>
2022-01-13 09:53:11 -0600

    add optional arg process_name

--- test/breaker_box_test.exs
+++ test/breaker_box_test.exs
@@ -10,231 +10,246 @@ defmodule BreakerBoxTest do
   @test_breaker_name "TestBreaker"
   @test_breaker_config %BreakerConfiguration{}
   @non_existent_breaker "NonExistentBreaker"
+  @process_name "Breakers"
 
   setup_all do
-    {:ok, _} = BreakerBox.start_link([])
+    {:ok, _} = BreakerBox.start_link([], @process_name)
 
     :ok
   end
 
   setup do
-    BreakerBox.register(@test_breaker_name, @test_breaker_config)
+    BreakerBox.register(@test_breaker_name, @test_breaker_config, @process_name)
 
     on_exit(fn ->
-      BreakerBox.remove(@test_breaker_name)
-      BreakerBox.remove(@non_existent_breaker)
+      BreakerBox.remove(@test_breaker_name, @process_name)
+      BreakerBox.remove(@non_existent_breaker, @process_name)
     end)
   end
 
   describe "disable/1" do
     test "breaker is disabled if it exists" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.disable(@test_breaker_name)
+      assert :ok = BreakerBox.disable(@test_breaker_name, @process_name)
 
       assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status(@test_breaker_name)
+               BreakerBox.status(@test_breaker_name, @process_name)
     end
 
     test "fails to disable a breaker that isn't there" do
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.disable(@non_existent_breaker)
+               BreakerBox.disable(@non_existent_breaker, @process_name)
     end
   end
 
   describe "enable/1" do
     test "breaker is enabled if it exists and is already enabled (noop)" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.enable(@test_breaker_name)
+      assert :ok = BreakerBox.enable(@test_breaker_name, @process_name)
     end
 
     test "breaker is enabled if it exists and was previously disabled" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.disable(@test_breaker_name)
+      assert :ok = BreakerBox.disable(@test_breaker_name, @process_name)
 
       assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status(@test_breaker_name)
+               BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.enable(@test_breaker_name)
+      assert :ok = BreakerBox.enable(@test_breaker_name, @process_name)
 
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
     end
 
     test "fails to disable a breaker that isn't there" do
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.enable(@non_existent_breaker)
+               BreakerBox.enable(@non_existent_breaker, @process_name)
     end
   end
 
   describe "register/2" do
     test "registers a breaker with the system" do
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
-      assert :ok = BreakerBox.register(@non_existent_breaker, @test_breaker_config)
+      assert :ok = BreakerBox.register(@non_existent_breaker, @test_breaker_config, @process_name)
 
-      assert {:ok, @non_existent_breaker} = BreakerBox.status(@non_existent_breaker)
+      assert {:ok, @non_existent_breaker} =
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
       expected_config = @test_breaker_config
 
-      assert {:ok, ^expected_config} = BreakerBox.get_config(@non_existent_breaker)
+      assert {:ok, ^expected_config} = BreakerBox.get_config(@non_existent_breaker, @process_name)
 
-      assert :ok = BreakerBox.remove(@non_existent_breaker)
+      assert :ok = BreakerBox.remove(@non_existent_breaker, @process_name)
 
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
     end
 
     test "re-registering a breaker will override the original configuration" do
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
-      assert :ok = BreakerBox.register(@non_existent_breaker, @test_breaker_config)
+      assert :ok = BreakerBox.register(@non_existent_breaker, @test_breaker_config, @process_name)
 
-      assert {:ok, @non_existent_breaker} = BreakerBox.status(@non_existent_breaker)
+      assert {:ok, @non_existent_breaker} =
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
       expected_config = @test_breaker_config
       new_config = BreakerConfiguration.reset_after_milliseconds(@test_breaker_config, 999_999)
 
-      assert {:ok, ^expected_config} = BreakerBox.get_config(@non_existent_breaker)
+      assert {:ok, ^expected_config} = BreakerBox.get_config(@non_existent_breaker, @process_name)
 
-      assert :ok = BreakerBox.register(@non_existent_breaker, new_config)
+      assert :ok = BreakerBox.register(@non_existent_breaker, new_config, @process_name)
 
-      assert {:ok, @non_existent_breaker} = BreakerBox.status(@non_existent_breaker)
+      assert {:ok, @non_existent_breaker} =
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
-      assert {:ok, ^new_config} = BreakerBox.get_config(@non_existent_breaker)
+      assert {:ok, ^new_config} = BreakerBox.get_config(@non_existent_breaker, @process_name)
 
-      assert :ok = BreakerBox.remove(@non_existent_breaker)
+      assert :ok = BreakerBox.remove(@non_existent_breaker, @process_name)
 
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
+    end
+
+    test "registering a breaker with max_failures less then 2 returns error" do
+      invalid_breaker_config = Map.put(@test_breaker_config, :max_failures, 1)
+
+      assert {:error, "BreakerBox: max_failures must be greater than 1"} =
+               BreakerBox.register(@non_existent_breaker, invalid_breaker_config, @process_name)
     end
   end
 
   describe "registered/0" do
     test "shows all breakers registered in the system" do
-      assert is_map(BreakerBox.registered())
+      assert is_map(BreakerBox.registered(@process_name))
 
-      assert BreakerBox.registered() |> Map.has_key?(@test_breaker_name)
+      assert BreakerBox.registered(@process_name) |> Map.has_key?(@test_breaker_name)
 
       expected_config = @test_breaker_config
 
-      assert ^expected_config = BreakerBox.registered() |> Map.get(@test_breaker_name)
+      assert ^expected_config =
+               BreakerBox.registered(@process_name) |> Map.get(@test_breaker_name)
 
-      refute BreakerBox.registered() |> Map.has_key?(@non_existent_breaker)
+      refute BreakerBox.registered(@process_name) |> Map.has_key?(@non_existent_breaker)
     end
   end
 
   describe "reset/1" do
     test "resets a breaker that exists and is not blown (noop)" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.reset(@test_breaker_name)
+      assert :ok = BreakerBox.reset(@test_breaker_name, @process_name)
     end
 
     test "does not reset a breaker that doesn't exist" do
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.reset(@non_existent_breaker)
+               BreakerBox.reset(@non_existent_breaker, @process_name)
     end
 
     test "does not reset a breaker that has been manually disabled" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.disable(@test_breaker_name)
-
-      assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status(@test_breaker_name)
-
-      assert :ok = BreakerBox.reset(@test_breaker_name)
+      assert :ok = BreakerBox.disable(@test_breaker_name, @process_name)
 
       assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status(@test_breaker_name)
+               BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.enable(@test_breaker_name)
+      assert :ok = BreakerBox.reset(@test_breaker_name, @process_name)
 
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:error, {:breaker_tripped, @test_breaker_name}} =
+               BreakerBox.status(@test_breaker_name, @process_name)
+
+      assert :ok = BreakerBox.enable(@test_breaker_name, @process_name)
+
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
     end
 
     test "resets a breaker that has been blown by exceeding the error limit" do
       {breaker_name, %BreakerConfiguration{max_failures: failures} = breaker_config} =
         StrictBreaker.registration()
 
-      assert :ok = BreakerBox.register(breaker_name, breaker_config)
+      assert :ok = BreakerBox.register(breaker_name, breaker_config, @process_name)
 
       1..failures
       |> Enum.each(fn _ ->
-        assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name)
-        assert :ok = BreakerBox.increment_error(breaker_name)
+        assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name, @process_name)
+        assert :ok = BreakerBox.increment_error(breaker_name, @process_name)
       end)
 
-      assert {:error, {:breaker_tripped, ^breaker_name}} = BreakerBox.status(breaker_name)
+      assert {:error, {:breaker_tripped, ^breaker_name}} =
+               BreakerBox.status(breaker_name, @process_name)
 
-      assert :ok = BreakerBox.reset(breaker_name)
+      assert :ok = BreakerBox.reset(breaker_name, @process_name)
 
-      assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name)
+      assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name, @process_name)
     end
   end
 
   describe "status/0" do
     test "returns the status of all breakers in the system" do
-      assert BreakerBox.status() |> Map.has_key?(@test_breaker_name)
+      assert BreakerBox.all_statuses(@process_name) |> Map.has_key?(@test_breaker_name)
 
-      assert {:ok, @test_breaker_name} = BreakerBox.status() |> Map.get(@test_breaker_name)
+      assert {:ok, @test_breaker_name} =
+               BreakerBox.all_statuses(@process_name) |> Map.get(@test_breaker_name)
 
-      refute BreakerBox.status() |> Map.has_key?(@non_existent_breaker)
+      refute BreakerBox.all_statuses(@process_name) |> Map.has_key?(@non_existent_breaker)
     end
 
     test "returns the correct status of all breakers" do
-      assert :ok = BreakerBox.disable(@test_breaker_name)
+      assert :ok = BreakerBox.disable(@test_breaker_name, @process_name)
 
       assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status(@test_breaker_name)
+               BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert BreakerBox.status() |> Map.has_key?(@test_breaker_name)
+      assert BreakerBox.all_statuses(@process_name) |> Map.has_key?(@test_breaker_name)
 
       assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status() |> Map.get(@test_breaker_name)
+               BreakerBox.all_statuses(@process_name) |> Map.get(@test_breaker_name)
 
-      assert :ok = BreakerBox.enable(@test_breaker_name)
+      assert :ok = BreakerBox.enable(@test_breaker_name, @process_name)
 
-      assert BreakerBox.status() |> Map.has_key?(@test_breaker_name)
+      assert BreakerBox.all_statuses(@process_name) |> Map.has_key?(@test_breaker_name)
 
-      assert {:ok, @test_breaker_name} = BreakerBox.status() |> Map.get(@test_breaker_name)
+      assert {:ok, @test_breaker_name} =
+               BreakerBox.all_statuses(@process_name) |> Map.get(@test_breaker_name)
     end
   end
 
   describe "status/1" do
     test "returns the status of a single breaker in the system" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
     end
 
     test "returns the current status of a breaker" do
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.disable(@test_breaker_name)
+      assert :ok = BreakerBox.disable(@test_breaker_name, @process_name)
 
       assert {:error, {:breaker_tripped, @test_breaker_name}} =
-               BreakerBox.status(@test_breaker_name)
+               BreakerBox.status(@test_breaker_name, @process_name)
 
-      assert :ok = BreakerBox.enable(@test_breaker_name)
+      assert :ok = BreakerBox.enable(@test_breaker_name, @process_name)
 
-      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name)
+      assert {:ok, @test_breaker_name} = BreakerBox.status(@test_breaker_name, @process_name)
     end
 
     test "errors if the breaker doesn't exist in the system" do
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
     end
   end
 
@@ -249,7 +264,7 @@ defmodule BreakerBoxTest do
              end) =~ expected_error_message
 
       assert {:error, {:breaker_not_found, @non_existent_breaker}} =
-               BreakerBox.status(@non_existent_breaker)
+               BreakerBox.status(@non_existent_breaker, @process_name)
 
       expected_error_message =
         "BreakerBox: :some_atom failed to register " <>
@@ -259,7 +274,8 @@ defmodule BreakerBoxTest do
                assert {:ok, %{}} = BreakerBox.init([:some_atom])
              end) =~ expected_error_message
 
-      assert {:error, {:breaker_not_found, :some_atom}} = BreakerBox.status(:some_atom)
+      assert {:error, {:breaker_not_found, :some_atom}} =
+               BreakerBox.status(:some_atom, @process_name)
     end
 
     test "fails to add circuit breaker module if it does not implement behaviour" do
@@ -273,7 +289,7 @@ defmodule BreakerBoxTest do
              end) =~ expected_error_message
 
       assert {:error, {:breaker_not_found, MisbehavingBreaker}} =
-               BreakerBox.status(MisbehavingBreaker)
+               BreakerBox.status(MisbehavingBreaker, @process_name)
     end
 
     test "initializes properly if circuit breaker module does implement behaviour" do
@@ -287,11 +303,12 @@ defmodule BreakerBoxTest do
 
       assert ^expected_config = Map.get(state, breaker_name)
 
-      assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name)
+      assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name, @process_name)
 
-      assert :ok = BreakerBox.remove(breaker_name)
+      assert :ok = BreakerBox.remove(breaker_name, @process_name)
 
-      assert {:error, {:breaker_not_found, ^breaker_name}} = BreakerBox.status(breaker_name)
+      assert {:error, {:breaker_not_found, ^breaker_name}} =
+               BreakerBox.status(breaker_name, @process_name)
     end
   end
 
@@ -300,15 +317,16 @@ defmodule BreakerBoxTest do
       {breaker_name, %BreakerConfiguration{max_failures: failures} = breaker_config} =
         StrictBreaker.registration()
 
-      assert :ok = BreakerBox.register(breaker_name, breaker_config)
+      assert :ok = BreakerBox.register(breaker_name, breaker_config, @process_name)
 
       1..failures
       |> Enum.each(fn _ ->
-        assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name)
-        assert :ok = BreakerBox.increment_error(breaker_name)
+        assert {:ok, ^breaker_name} = BreakerBox.status(breaker_name, @process_name)
+        assert :ok = BreakerBox.increment_error(breaker_name, @process_name)
       end)
 
-      assert {:error, {:breaker_tripped, ^breaker_name}} = BreakerBox.status(breaker_name)
+      assert {:error, {:breaker_tripped, ^breaker_name}} =
+               BreakerBox.status(breaker_name, @process_name)
     end
   end
 end
